db813b9eb772fabe5c2987a402f39e3d0993c78b
Victor Berton <bertonvictor@hotmail.com>
2025-06-24 13:13:17 -0300

    config file

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin.default(config)],
+      }));
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.feature",
+    defaultCommandTimeout: 5000,
+    viewportWidth: 1440,
+    viewportHeight: 800,
+    baseUrl: "https://storefront.dev.gscdigital.com.br",
+  },
+});
